8a2c9c19f84eb3b3662227b506fda3cde4e0d199
Krzee <derekhunziker@gmail.com>
2023-06-02 19:07:28 -0400

    battle sequence
    attacks, health moving, win state

--- turn based combat stuff/Assets/BattleSystem.cs
+++ turn based combat stuff/Assets/BattleSystem.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UIElements;
 using UnityEngine.UI;
 
-public enum BattleState { START, PLAYERTURN, ENEMYTURN, WON, LOST }
+public enum BattleState { START, PLAYERTURN, ENEMYTURN, WON, LOST, SEQUENCE }
 
 public class BattleSystem : MonoBehaviour
 {
@@ -26,10 +26,10 @@ public class BattleSystem : MonoBehaviour
     void Start()
     {
         state = BattleState.START;
-        SetupBattle();
+        StartCoroutine(SetupBattle());
     }
 
-    void SetupBattle() 
+    IEnumerator SetupBattle() 
     {
         GameObject playerGO = Instantiate(playerPrefab, playerStation);
         playerUnit = playerGO.GetComponent<Unit>();
@@ -41,6 +41,89 @@ public class BattleSystem : MonoBehaviour
 
         playerHUD.SetHUD(playerUnit);
         enemyHUD.SetHUD(enemyUnit);
+
+        yield return new WaitForSeconds(3f);
+
+        state = BattleState.PLAYERTURN;
+        PlayerTurn();
     }
-        
+       
+    IEnumerator PlayerAttack()
+    {
+        state = BattleState.SEQUENCE;
+
+        bool isDead = enemyUnit.TakeDamage(playerUnit.damage);
+
+        enemyHUD.SetHP(enemyUnit.currentHP);
+        dialogueText.text = playerUnit.unitName + " did " + playerUnit.damage + " damage!";
+
+        yield return new WaitForSeconds(2f);
+
+        if (isDead)
+        {
+            state = BattleState.WON;
+            EndBattle();
+        }
+        else
+        {
+            state = BattleState.ENEMYTURN;
+            StartCoroutine(EnemyTurn());
+        }
+
+    }
+
+    void EndBattle()
+    {
+        if (state == BattleState.WON)
+        {
+            dialogueText.text = "You defeated " + enemyUnit.unitName + "!";
+        }
+        else if (state == BattleState.LOST) 
+        {
+            dialogueText.text = playerUnit.unitName + " has fainted!";
+        }
+    }
+
+    IEnumerator EnemyTurn()
+    {
+
+        dialogueText.text = enemyUnit.unitName + " attacks!";
+
+        yield return new WaitForSeconds(2f);
+
+
+        bool isDead = playerUnit.TakeDamage(enemyUnit.damage);
+
+        playerHUD.SetHP(playerUnit.currentHP);
+
+        dialogueText.text = enemyUnit.unitName + " did " + enemyUnit.damage + " damage!";
+
+        yield return new WaitForSeconds(2f);
+
+        if (isDead)
+        {
+            state = BattleState.LOST;
+            EndBattle();
+        }
+        else
+        {
+            state = BattleState.PLAYERTURN;
+            PlayerTurn();
+        }
+    }
+
+    void PlayerTurn() 
+    {
+        dialogueText.text = "Choose an action:";
+    }
+
+    public void OnAttackButton()
+    {
+        if (state != BattleState.PLAYERTURN)
+        {
+            return;
+        }
+        StartCoroutine(PlayerAttack());
+
+    }
 }
